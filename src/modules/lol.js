9036fa9973839e6d04a07e692f7ddd58b0f91ebc
vemu <kamilox26@gmail.com>
2019-11-06 19:48:03 +0100

    fix

--- src/modules/lol.js
+++ src/modules/lol.js
@@ -3,12 +3,12 @@
 const LeagueJS = require(`leaguejs`);
 
 
-const league_api_key = `RGAPI-56dc0a6d-5706-48bb-83b5-02ebbd97c387`,
+const league_api_key = `RGAPI-e23bf58f-bd6d-4174-85a9-66901d628722`,
     leagueJs = new LeagueJS(league_api_key);
 
 module.exports = async (_message, _api, split) => {
     let region = ``;
-    split[2] == `euw` ? region = `euw` : region = `eune`;
+    split[2] == `euw` ? region = `euw1` : region = `eun1`;
     const {id, name, summonerLevel, revisionDate} = await leagueJs.Summoner.gettingByName(split[1], region);
     const rank = await leagueJs.League.gettingEntriesForSummonerId(id, region);
     const ranks = rank.reduce((acc, value) => {

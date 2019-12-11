810a2940908539a912a4e67f66a933c32bcb8d38
vemu <kamilox26@gmail.com>
2019-12-11 19:31:12 +0100

    fix

--- src/modules/miejski.js
+++ src/modules/miejski.js
@@ -1,7 +1,8 @@
 `use strict`;
 
 const request = require(`request-promise-native`),
-    cheerio = require(`cheerio`);
+    cheerio = require(`cheerio`),
+    utf8 = require(`utf8`);
 
 
 module.exports = async (_message, _api, split) => {
@@ -9,7 +10,7 @@ module.exports = async (_message, _api, split) => {
     if (split.length >= 1) {
         let url = `https://www.miejski.pl/slowo-`;
         url += split.reduce((acc, value) => acc += `${value}+`, ``).slice(0, -1);
-        $ = await request({ uri: url, transform: body => cheerio.load(body) });
+        $ = await request({ uri: utf8.encode(url), transform: body => cheerio.load(body) });
     } else {
         $ = await request({ uri: `https://www.miejski.pl/losuj`, followAllRedirects: true, transform: body => cheerio.load(body) });
     }

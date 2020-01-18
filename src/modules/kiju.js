731fe63a61c88c102effa11481c5cdc631ed6f39
vemu <kamilox26@gmail.com>
2020-01-18 14:39:10 +0100

    added !kiju

---
+++ src/modules/kiju.js
@@ -0,0 +1,12 @@
+`use strict`;
+
+const fs = require(`fs`).promises;
+
+module.exports = async (message, api, split) => {
+    let count = parseInt(await fs.readFile(`kiju.json`, `utf-8`));
+    if (split.length == 1 && (message.senderID == `100003748210938` || `100038916831294` || api.getCurrentUserID())) {
+        count += parseInt(split);
+        await fs.writeFile(`kiju.json`, count);
+    }
+    return count.toString();
+};
